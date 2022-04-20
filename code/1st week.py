e315b5ee29b4a0952ad21b4ec0d2ef1d7a260964
ZoonPark312 <70933580+JYPark312@users.noreply.github.com>
2022-04-20 17:51:32 +0900

    Add files via upload

--- code/1st week.py
+++ code/1st week.py
@@ -1,29 +1,80 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 13 13:11:07 2021
 
-@author: young
-"""
+# Use only following packages
 
-import pandas as pd\
-from sklearn.linear_model import LinearRegression    
+import numpy as np
+from scipy import stats
+from sklearn.datasets import load_boston
 
-petrol = pd.read_csv("https://drive.google.com/uc?export=download&id=1R9B0D_fSjfCiSaS1WWEjEbSHFOXlVjQA")
+def ftest(X,y):
+    # X: inpute variables
+    # y: target
+    X=data.data
+    n, p= X.shape
+    X_data = np.concatenate((np.ones(n).reshape(-1,1), X), axis=1) #X matrix
+    X_T_X = np.matmul(X_data.T, X_data) # X transpose X
+    beta = np.matmul(np.matmul(np.linalg.inv(X_T_X), X_data.T), y.reshape(-1, 1)) #beta 값
+    y_hat = np.matmul(X_data, beta) #y_hat value
+    
+    SSE = sum((y.reshape(-1, 1) - y_hat)**2)
+    SSR = sum((y_hat - np.mean(y_hat))**2)
+    SST = SSR + SSE
+    MSR = SSR / p
+    MSE = SSE / (n - p - 1)
+    f_value = MSR / MSE
+    p_value = 1- stats.f.cdf(f_value, p, n - p - 1)
 
-X = petrol[['tax', 'income', 'highway', 'license']]
-y=petrol['consumption']
+    print("----------------------------------------------------------------------------------------------------")
+    print("Factor      Sum of square    Degree of Freedom      Mean Square         F-value           pr>F")
+    print("Model", "   ",SSR, "        ", p, "         ", MSR, "  ", f_value, "  ", p_value) 
+    print("Error", "   ",SSE, "        ", n - p - 1, "         ", MSE)
+    print("----------------------------------------------------------------------------------------------------")
+    print("Total", "   ",SSE + SSR, "        ", p + n - p - 1)
+    print("----------------------------------------------------------------------------------------------------")
+    return 0
 
-lr=LinearRegression()
+def ttest(X,y,varname=None):
+    # X: inpute variables
+    # y: target
+    X=data.data
+    name = np.append('const',data.feature_names)
+    n, p= X.shape
+    X_data = np.concatenate((np.ones(n).reshape(-1,1), X), axis=1)
+    X_T_X = np.matmul(X_data.T, X_data)
+    beta = np.matmul(np.matmul(np.linalg.inv(X_T_X), X_data.T), y.reshape(-1, 1))
+    y_hat = np.matmul(X_data, beta)
 
-lr.fit(X,y)
+    SSE = sum((y.reshape(-1, 1) - y_hat)**2)
+    SSR = sum((y_hat - np.mean(y_hat))**2)
+    SST = SSR + SSE
+    MSR = SSR / p
+    MSE = SSE / (n - p - 1)
 
-lr.coef_ #coefficient
-lr.intercept_ #beta
+    se_matrix = MSE*(np.linalg.inv(np.matmul(X_data.T,X_data))) #sematrix
+    se_matrix = np.diag(se_matrix) #sematrix 대각성분
+    se = np.sqrt(se_matrix)
 
-y_pred = lr.predict(X) #예측값
+    t_value = []
+    for i in range(len(se_matrix)):
+        t_value.append((beta[i] / np.sqrt(se_matrix[i])))
 
-error = y-y_pred #error값
+    p_value = ((1 - stats.t.cdf(np.abs(np.array(t_value)), n - p - 1)) * 2)
 
-lr.score(X, y)
+    print("----------------------------------------------------------------------------------------------------")
+    print("Variable         coef                     se                   t                   Pr>|t|")
+
+    for i in range(0,14):
+        print(name[i], "       ", beta[i], "     ", se[i], "     ", t_value[i], "     ", p_value[i])
+    print("----------------------------------------------------------------------------------------------------")    
+    return 0
+
+## Do not change!
+# load data
+data=load_boston()
+X=data.data
+y=data.target
+
+ftest(X,y)
+ttest(X,y,varname=data.feature_names)
 
 
